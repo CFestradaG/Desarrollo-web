4e5c6b6c05b23f218fe4cce55d5e36a3ad84f82e
FRANCISCO ESTRADA <161900461+CFestradaG@users.noreply.github.com>
2026-08-29 12:13:23 -0600

    Add desarrollo.js with student logging

---
+++ desarrollo.js
@@ -0,0 +1,18 @@
+console.log("Hola desde Node.js");
+
+/*
+const curso = "Desarrollo";
+const universidad = "UMG";
+console.log(`Curso: ${curso}`);
+console.log(`Universidad: ${universidad}`);
+*/
+
+const estudiantes = [
+    "Ana",
+    "Carlos",
+    "María"
+];
+
+estudiantes.forEach((estudiante) => {
+    console.log(estudiante);
+});
