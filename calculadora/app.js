c591c1f033399351bb45951998400c846d6a5d7b
FRANCISCO ESTRADA <161900461+CFestradaG@users.noreply.github.com>
2026-09-22 04:58:07 +0000

    se inicia la tarea 2

---
+++ calculadora/app.js
@@ -0,0 +1,8 @@
+// app.js
+
+const { sumar, restar, multiplicar, dividir } = require("./operaciones");
+
+console.log(sumar(20, 10));        // 30
+console.log(restar(20, 10));       // 10
+console.log(multiplicar(20, 10));  // 200
+console.log(dividir(20, 10));      // 2
